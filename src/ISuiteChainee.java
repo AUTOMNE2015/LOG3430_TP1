710c970c6a9a392ad02782ae2f80b073f5f5b3ea
dbqt <david.tran@polymtl.ca>
2015-09-12 13:36:46 -0400

    interface update to follow suitechainee

--- src/ISuiteChainee.java
+++ src/ISuiteChainee.java
@@ -1,8 +1,8 @@
 public interface ISuiteChainee {
 	void add(ElementSuite nouvelElement);
 	void removeAt(int position);
-	void removeItem(ElementSuite element);
-	void setAt(ElementSuite nouvelElement, int position);
+	void removeItem(int element);
+	void setAt(int nouvelElement, int position);
 	ElementSuite getAt(int position);
 	int getSize();
 	void reset();
