bfae41f0d0018434d2c14188da68585ad505683b
chtref <charles-2.tremblay@polymtl.ca>
2015-10-02 20:51:03 -0400

    On charge le vieux contenu lorsque estVide False

--- src/TestEC.java
+++ src/TestEC.java
@@ -1,5 +1,11 @@
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -24,13 +30,43 @@ public class TestEC {
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 	}
+	
+	private String loadOldContent(String filename){
+		// Essai d'ouvrir un stream du fichier.
+        Properties properties = new Properties();
+		File file = new File(filename);
+		FileInputStream fileInput;
+		try 
+		{	
+			fileInput = new FileInputStream(file);
+			properties.load(fileInput);
+		} 
+		catch (FileNotFoundException e)
+		{
+			// Fichier introuvable, on va créer un nouveau fichier plus tard.
+			System.out.println("Fichier introuvable.");
+			return null;
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+			return null;
+		}
+		String oldContent = properties.getProperty("contenu");
+		if(!(oldContent == null || oldContent.isEmpty()))
+		{
+			oldContent += ", ";
+		}
+
+		return oldContent;
+	}
 
 	@Test
 	public void testEC1() {
 		ISuiteChainee suite;
 		try {
 			suite = new SuiteChainee("testec1.properties", "addition", 1, 2, 1, true);
-			assertTrue("testEC1 : La liste n'est pas valide.",suite.isValid());
+			assertTrue("testEC1 : La liste n'est pas valide.", suite.toString().equals("1"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -41,7 +77,7 @@ public class TestEC {
 	public void testEC2() {
 		try{
 		ISuiteChainee suite = new SuiteChainee("testec2.properties", "soustraction", 2, 1, 2, true);
-		assertTrue("testEC2 : La liste n'est pas valide.",suite.isValid());
+		assertTrue("testEC2 : La liste n'est pas valide.", suite.toString().equals("2, 1"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -50,8 +86,9 @@ public class TestEC {
 	@Test
 	public void testEC3() {
 		try{
-		ISuiteChainee suite = new SuiteChainee("testec3.properties", "multiplication", 1, 2, 5, false);
-		assertTrue("testEC3 : La liste n'est pas valide.",!suite.isValid());
+			String vieuxContenu = loadOldContent("testec3.properties");
+			ISuiteChainee suite = new SuiteChainee("testec3.properties", "multiplication", 1, 2, 5, false);
+			assertTrue("testEC3 : La liste n'est pas valide.", suite.toString().equals(vieuxContenu + "1, 2, 2, 4, 8"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
